eca0df100cf321d7f8e0764863273df9c77e3a9f
muth <muthunguclintn@gmail.com>
2021-04-28 16:03:24 +0300

    commit

--- birds.js
+++ birds.js
@@ -1,6 +1,45 @@
-var birdimages = document.querySelectorAll("#birds-gallery figure");
-for(i=0; i<birdimages.length; i++) {
-    birdimages[i].addEventListener('click', function() {
-        this.classList.toggle("expanded"); birds-gallery.classList.toggle("full")
-    });
+function myGallery() {
+    var x = document.getElementById("slideshow");
+    if (x.style.display == "none") {
+        x.style.display = "block";
+    }
+    else {
+        x.style.display = "none";
+    }
+}
+function myGallery2() {
+    var x = document.getElementById("slideshow2");
+    if (x.style.display == "none") {
+        x.style.display = "block";
+    }
+    else {
+        x.style.display = "none";
+    }
+}
+function myGallery3() {
+    var x = document.getElementById("slideshow3");
+    if (x.style.display == "none") {
+        x.style.display = "block";
+    }
+    else {
+        x.style.display = "none";
+    }
+}
+function myGallery4() {
+    var x = document.getElementById("slideshow4");
+    if (x.style.display == "none") {
+        x.style.display = "block";
+    }
+    else {
+        x.style.display = "none";
+    }
+}
+function myGallery5() {
+    var x = document.getElementById("slideshow5");
+    if (x.style.display == "none") {
+        x.style.display = "block";
+    }
+    else {
+        x.style.display = "none";
+    }
 }
