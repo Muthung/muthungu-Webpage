ea2c81621c3a590d1d456648b0ff201ec3b58e8d
muth <muthunguclintn@gmail.com>
2021-05-01 22:50:03 +0300

    initial commit

--- legend.js
+++ legend.js
@@ -1,7 +1,7 @@
 // Chadwick //
 
 function myLegend1() {
-    var a = document.getElementById("slideshow2");
+    var a = document.getElementById("slideshow");
     if(a.style.display == "none") {
        a.style.display = "block";
     }
@@ -9,3 +9,40 @@ function myLegend1() {
        a.style.display = "none";
     }
 }
+
+// Bob //
+
+function myLegend2() {
+   var a = document.getElementById("slideshow2");
+   if(a.style.display == "none") {
+      a.style.display = "block";
+   }
+   else {
+      a.style.display = "none";
+   }
+}
+
+// 2 Pac //
+
+function myLegend3() {
+   var a = document.getElementById("slideshow3");
+   if(a.style.display == "none") {
+      a.style.display = "block";
+   }
+   else {
+      a.style.display = "none";
+   }
+}
+
+// Big Poppa //
+
+function myLegend4() {
+   var a = document.getElementById("slideshow4");
+   if(a.style.display == "none") {
+      a.style.display = "block";
+   }
+   else {
+      a.style.display = "none";
+   }
+}
+
