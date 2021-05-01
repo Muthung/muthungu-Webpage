5043892e2293c070c68aa7435f8d71c299d60863
muth <muthunguclintn@gmail.com>
2021-05-01 13:21:49 +0300

    initial commit

---
+++ legend.js
@@ -0,0 +1,11 @@
+// Chadwick //
+
+function myLegend1() {
+    var a = document.getElementById("slideshow2");
+    if(a.style.display == "none") {
+       a.style.display = "block";
+    }
+    else {
+       a.style.display = "none";
+    }
+}
