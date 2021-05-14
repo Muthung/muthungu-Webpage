a9b281e535f976ed9a11f73ec0e91cb02d17c7a0
muth <muthunguclintn@gmail.com>
2021-05-14 07:46:01 +0300

    onclick

--- legend.js
+++ legend.js
@@ -2,11 +2,20 @@
 
 function myLegend1() {
     var a = document.getElementById("slideshow");
+    var b = document.getElementById("slideshow2");
+    var c = document.getElementById("slideshow3");
+    var d = document.getElementById("slideshow4");
     if(a.style.display == "none") {
        a.style.display = "block";
+       b.style.display = "none";
+       c.style.display = "none";
+       d.style.display = "none";
     }
     else {
        a.style.display = "none";
+       b.style.display = "block";
+       c.style.display = "block";
+       d.style.display = "block";
     }
 }
 
@@ -14,11 +23,20 @@ function myLegend1() {
 
 function myLegend2() {
    var a = document.getElementById("slideshow2");
+   var b = document.getElementById("slideshow");
+   var c = document.getElementById("slideshow3");
+   var d = document.getElementById("slideshow4");
    if(a.style.display == "none") {
       a.style.display = "block";
+      b.style.display = "none";
+      c.style.display = "none";
+      d.style.display = "none";
    }
    else {
       a.style.display = "none";
+      b.style.display = "block";
+      c.style.display = "block";
+      d.style.display = "block";
    }
 }
 
@@ -26,11 +44,20 @@ function myLegend2() {
 
 function myLegend3() {
    var a = document.getElementById("slideshow3");
+   var b = document.getElementById("slideshow");
+   var c = document.getElementById("slideshow2");
+   var d = document.getElementById("slideshow4");
    if(a.style.display == "none") {
       a.style.display = "block";
+      b.style.display = "none";
+      c.style.display = "none";
+      d.style.display = "none";
    }
    else {
       a.style.display = "none";
+      b.style.display = "block";
+      c.style.display = "block";
+      d.style.display = "block";
    }
 }
 
@@ -38,11 +65,20 @@ function myLegend3() {
 
 function myLegend4() {
    var a = document.getElementById("slideshow4");
+   var b = document.getElementById("slideshow");
+   var c = document.getElementById("slideshow2");
+   var d = document.getElementById("slideshow3");
    if(a.style.display == "none") {
       a.style.display = "block";
+      b.style.display = "none";
+      c.style.display = "none";
+      d.style.display = "none";
    }
    else {
       a.style.display = "none";
+      b.style.display = "block";
+      c.style.display = "block";
+      d.style.display = "block";
    }
 }
 
