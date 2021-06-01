e107f63afb12a3225a7f415dabef0d4af3c9152e
muth <muthunguclintn@gmail.com>
2021-06-01 19:08:07 +0300

    album commit

---
+++ albums/album.js
@@ -0,0 +1,196 @@
+// Onclick displays the Nas details and hides others //
+function myAlbum() {
+    var a = document.getElementById("artist-details");
+    var b = document.getElementById("artist1");
+    var c = document.getElementById("artist2");
+    var d = document.getElementById("artist3");
+    var e = document.getElementById("artist4");
+    var f = document.getElementById("artist5");
+    var g = document.getElementById("artist6");
+    if (a.style.display == "none") {
+        a.style.display = "block";
+        b.style.display = "none";
+        c.style.display = "none";
+        d.style.display = "none";
+        e.style.display = "none";
+        f.style.display = "none";
+        g.style.display = "none";
+    }
+    else {
+        a.style.display = "none";
+        b.style.display = "block";
+        c.style.display = "block";
+        d.style.display = "block";
+        e.style.display = "block";
+        f.style.display = "block";
+        g.style.display = "block";
+    }
+}
+// Onclick displays the Chronixx details and hides others //
+function myAlbum1() {
+    var a = document.getElementById("artist");
+    var b = document.getElementById("artist1-details");
+    var c = document.getElementById("artist2");
+    var d = document.getElementById("artist3");
+    var e = document.getElementById("artist4");
+    var f = document.getElementById("artist5");
+    var g = document.getElementById("artist6");
+    if (b.style.display == "none") {
+        b.style.display = "block";
+        a.style.display = "none";
+        c.style.display = "none";
+        d.style.display = "none";
+        e.style.display = "none";
+        f.style.display = "none";
+        g.style.display = "none";
+    }
+    else {
+        b.style.display = "none";
+        a.style.display = "block";
+        c.style.display = "block";
+        d.style.display = "block";
+        e.style.display = "block";
+        f.style.display = "block";
+        g.style.display = "block";
+    }
+}
+// Onclick displays the Jay-Z details and hides others //
+function myAlbum2() {
+    var a = document.getElementById("artist");
+    var b = document.getElementById("artist1");
+    var c = document.getElementById("artist2-details");
+    var d = document.getElementById("artist3");
+    var e = document.getElementById("artist4");
+    var f = document.getElementById("artist5");
+    var g = document.getElementById("artist6");
+    if (c.style.display == "none") {
+        c.style.display = "block";
+        a.style.display = "none";
+        b.style.display = "none";
+        d.style.display = "none";
+        e.style.display = "none";
+        f.style.display = "none";
+        g.style.display = "none";
+    }
+    else {
+        c.style.display = "none";
+        a.style.display = "block";
+        b.style.display = "block";
+        d.style.display = "block";
+        e.style.display = "block";
+        f.style.display = "block";
+        g.style.display = "block";
+    }
+}
+// Onclick displays the Nyashinski details and hides others //
+function myAlbum3() {
+    var a = document.getElementById("artist");
+    var b = document.getElementById("artist1");
+    var c = document.getElementById("artist2");
+    var d = document.getElementById("artist3-details");
+    var e = document.getElementById("artist4");
+    var f = document.getElementById("artist5");
+    var g = document.getElementById("artist6");
+    if (d.style.display == "none") {
+        d.style.display = "block";
+        a.style.display = "none";
+        b.style.display = "none";
+        c.style.display = "none";
+        e.style.display = "none";
+        f.style.display = "none";
+        g.style.display = "none";
+    }
+    else {
+        d.style.display = "none";
+        a.style.display = "block";
+        b.style.display = "block";
+        c.style.display = "block";
+        e.style.display = "block";
+        f.style.display = "block";
+        g.style.display = "block";
+    }
+}
+// Onclick displays the Kendrick Lamar details and hides others //
+function myAlbum4() {
+    var a = document.getElementById("artist");
+    var b = document.getElementById("artist1");
+    var c = document.getElementById("artist2");
+    var d = document.getElementById("artist3");
+    var e = document.getElementById("artist4-details");
+    var f = document.getElementById("artist5");
+    var g = document.getElementById("artist6");
+    if (e.style.display == "none") {
+        e.style.display = "block";
+        a.style.display = "none";
+        b.style.display = "none";
+        c.style.display = "none";
+        d.style.display = "none";
+        f.style.display = "none";
+        g.style.display = "none";
+    }
+    else {
+        e.style.display = "none";
+        a.style.display = "block";
+        b.style.display = "block";
+        c.style.display = "block";
+        d.style.display = "block";
+        f.style.display = "block";
+        g.style.display = "block";
+    }
+}
+// Onclick displays the Protoje details and hides others //
+function myAlbum5() {
+    var a = document.getElementById("artist");
+    var b = document.getElementById("artist1");
+    var c = document.getElementById("artist2");
+    var d = document.getElementById("artist3");
+    var e = document.getElementById("artist4");
+    var f = document.getElementById("artist5-details");
+    var g = document.getElementById("artist6");
+    if (f.style.display == "none") {
+        f.style.display = "block";
+        a.style.display = "none";
+        b.style.display = "none";
+        c.style.display = "none";
+        d.style.display = "none";
+        e.style.display = "none";
+        g.style.display = "none";
+    }
+    else {
+        f.style.display = "none";
+        a.style.display = "block";
+        b.style.display = "block";
+        c.style.display = "block";
+        d.style.display = "block";
+        e.style.display = "block";
+        g.style.display = "block";
+    }
+}
+// Onclick displays the Eminem details and hides others //
+function myAlbum6() {
+    var a = document.getElementById("artist");
+    var b = document.getElementById("artist1");
+    var c = document.getElementById("artist2");
+    var d = document.getElementById("artist3");
+    var e = document.getElementById("artist4");
+    var f = document.getElementById("artist5");
+    var g = document.getElementById("artist6-details");
+    if (g.style.display == "none") {
+        g.style.display = "block";
+        a.style.display = "none";
+        b.style.display = "none";
+        c.style.display = "none";
+        d.style.display = "none";
+        e.style.display = "none";
+        f.style.display = "none";
+    }
+    else {
+        g.style.display = "none";
+        a.style.display = "block";
+        b.style.display = "block";
+        c.style.display = "block";
+        d.style.display = "block";
+        e.style.display = "block";
+        f.style.display = "block";
+    }
+}
