5b399884bd9e62a845fbc4d53aac8cf12ba5ff98
muth <muthunguclintn@gmail.com>
2021-06-01 19:08:51 +0300

    legends commit

---
+++ legends/legend.js
@@ -0,0 +1,72 @@
+// Chadwick //
+
+function myLegend1() {
+    var a = document.getElementById("slideshow");
+    var b = document.getElementById("slideshow2");
+    var c = document.getElementById("slideshow3");
+    var d = document.getElementById("slideshow4");
+    if(a.style.display == "none") {
+       a.style.display = "block";
+       b.style.display = "none";
+       c.style.display = "none";
+       d.style.display = "none";
+    }
+    else {
+       a.style.display = "none";
+    }
+}
+
+// Bob //
+
+function myLegend2() {
+   var a = document.getElementById("slideshow2");
+   var b = document.getElementById("slideshow");
+   var c = document.getElementById("slideshow3");
+   var d = document.getElementById("slideshow4");
+   if(a.style.display == "none") {
+      a.style.display = "block";
+      b.style.display = "none";
+      c.style.display = "none";
+      d.style.display = "none";
+   }
+   else {
+      a.style.display = "none";
+   }
+}
+
+// 2 Pac //
+
+function myLegend3() {
+   var a = document.getElementById("slideshow3");
+   var b = document.getElementById("slideshow");
+   var c = document.getElementById("slideshow2");
+   var d = document.getElementById("slideshow4");
+   if(a.style.display == "none") {
+      a.style.display = "block";
+      b.style.display = "none";
+      c.style.display = "none";
+      d.style.display = "none";
+   }
+   else {
+      a.style.display = "none";
+   }
+}
+
+// Big Poppa //
+
+function myLegend4() {
+   var a = document.getElementById("slideshow4");
+   var b = document.getElementById("slideshow");
+   var c = document.getElementById("slideshow2");
+   var d = document.getElementById("slideshow3");
+   if(a.style.display == "none") {
+      a.style.display = "block";
+      b.style.display = "none";
+      c.style.display = "none";
+      d.style.display = "none";
+   }
+   else {
+      a.style.display = "none";
+   }
+}
+
